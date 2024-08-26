2aa2ca8db471dfc40d03a45cf949ecd96264855b
Mario Kreitz <Mario.Kreitz@web.de>
2024-08-26 09:25:20 +0200

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { FlipWords } from "./ui/flip-words";
 import { LinkPreview } from "./ui/link-preview";
 import { AnimatedTooltip } from "./ui/animated-tooltip";
+import { FloatingDock } from "./ui/floating-dock";
+import { IconBrandGithub } from "@tabler/icons-react";
 
 export function Hero() {
   const words = ["bessere", "coole", "schöne", "moderne"];
@@ -9,8 +11,23 @@ export function Hero() {
     {
       id: 1,
       name: "Mario Kreitz",
-      designation: "Frontend Developer",
+      designation: "Lead - Frontend Developer",
       image: "/img/profile_mario.JPEG",
+    },
+    {
+      id: 2,
+      name: "Marcel Schmidt",
+      designation: "Projekt Partner - Frontend Developer",
+      image: "/img/profile_marcel.jpeg",
+    },
+  ];
+  const links = [
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "https://github.com/mariokreitz",
     },
   ];
 
@@ -19,10 +36,15 @@ export function Hero() {
       <div className="mb-10 flex w-full flex-row items-center justify-center">
         <AnimatedTooltip items={people} />
       </div>
+      <FloatingDock
+        desktopClassName="fixed bottom-5 right-5"
+        mobileClassName="fixed bottom-5 right-5"
+        items={links}
+      />
       <div className="mx-auto text-4xl font-normal">
         Erstelle
         <FlipWords words={words} /> <br />
-        Webseiten in der{" "}
+        Webseiten mit der{" "}
         <LinkPreview
           url="https://developerakademie.com/"
           className="text-dev-aka"
